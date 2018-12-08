cb7e1aea0f8cc14d6b094a6ef6d279ba859d0eca
Ivan <imeshcheriyakov@gmail.com>
2018-12-08 17:43:08 +0300

    Revert "в инициализацию фикстуры вынесен логин"
    This reverts commit 9fdfd94f75406c420d861a404b867778e137e0a4.

--- addresbook-web-tests/src/test/java/ru/stqa/pft/adressbook/GroupCreationTests.java
+++ addresbook-web-tests/src/test/java/ru/stqa/pft/adressbook/GroupCreationTests.java
@@ -16,7 +16,11 @@ public class  GroupCreationTests{
     @BeforeMethod
     public void setUp() throws Exception {
         wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-        wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    }
+    
+    @Test
+    public void GroupCreationTests() {
         wd.get("http://localhost:8080/addressbook/");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).click();
@@ -26,11 +30,6 @@ public class  GroupCreationTests{
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-    }
-    
-    @Test
-    public void testGroupCreation() {
-
         wd.findElement(By.cssSelector("html")).click();
         wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
